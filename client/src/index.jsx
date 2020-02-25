129e33eb1a37f818f0dea2014971dee35b0b27a1
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-02-25 18:17:04 -0300

    Feature(project-base): Project base created

---
+++ client/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <section id="app">HelloWorld</section>
+  );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
